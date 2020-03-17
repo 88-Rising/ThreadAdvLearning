84f9ad99711f0a6b4e166f878795788242300d4b
88-Rising <348514635@qq.com>
2020-03-17 15:17:21 +0800

    单例模式下双重校验锁(DCL)

--- src/main/java/UnsafeThread.java
+++ src/main/java/UnsafeThread.java
@@ -1,3 +1,32 @@
+class Sington{
+    private static final Sington sington1=new Sington();
+
+    private Sington(){
+
+    }
+
+    public static Sington getInstance(){
+        return sington1;
+    }
+
+    private static volatile Sington sington2=null;
+/*
+* 执行双重检查是因为，如果多个线程同时了通过了第一次检查，并且其中一个线程首先通过了第二次检查并实例化了对象，那么剩余通过了第一次检查的线程就不会再去实例化对象。
+*这样，除了初始化的时候会出现加锁的情况，后续的所有调用都会避免加锁而直接返回，解决了性能消耗的问题。
+* */
+    public static Sington getInstance2(){
+        if(sington2==null){
+            synchronized (Sington.class) {
+                if(sington2==null) {
+                    sington2 = new Sington();
+                }
+            }
+        }
+        return sington2;
+    }
+
+}
+
 public class UnsafeThread {
 
     public static volatile int COUNT;
@@ -21,6 +50,9 @@ public class UnsafeThread {
                     }
                 }
             }).start();
+        }
+        while(Thread.activeCount()>1){
+            Thread.yield();
         }
     }
 }
